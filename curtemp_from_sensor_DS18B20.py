b5abfc28a9ce2e429fc2abea8c5022c54c0a54dc
zikunliu <31624721+zikunliu@users.noreply.github.com>
2018-05-27 15:37:33 -0400

    Delete curtemp_from_sensor_DS18B20.py

--- curtemp_from_sensor_DS18B20.py
+++
@@ -1,6 +0,0 @@
-from w1thermsensor import W1ThermSensor
-def get_temp_from_sensor():
-        sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "041703ee74ff")
-        temperature_in_celsius = sensor.get_temperature()
-        temp=int(32+temperature_in_celsius*1.8)
-        return temp
